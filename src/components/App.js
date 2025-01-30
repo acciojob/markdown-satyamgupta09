cc1b8fe300826eb6afe8cc37d3bd653f8ae4ecae
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-31 03:27:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,27 @@
 import { useEffect, useState } from "react";
-import "./../styles/App.css";
+import "./styles.css";
 
 export default function App() {
   const [data, setData] = useState("");
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 1000);
-    return () => clearTimeout(timer);
-  }, []);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => setLoading(false), 1000);
+  //   return () => clearTimeout(timer);
+  // }, []);
+  handleChange = (e) => {
+    setData(e.target.value);
+    setLoading(false);
+  };
 
   return (
-    <div className="App">
-      <div className="container">
-        <div className="section left">
-          <textarea
-            type="text"
-            className="textarea"
-            onChange={(e) => setData(e.target.value)}
-          />
-        </div>
-        {loading ? (
-          <p className="loading">Loading...</p>
-        ) : (
-          <div className="section right preview">{data}</div>
-        )}
-      </div>
+    <div className="App">]
+      <textarea type="text" className="textarea" onChange={handleChange} />
+      {loading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        <div className="preview">{data}</div>
+      )}
     </div>
   );
 }
